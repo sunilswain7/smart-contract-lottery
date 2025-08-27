374ea15a5097e0719dbf1e3a130902ae2ab67251
sunilswain7 <sunilkai320@gmail.com>
2025-08-27 21:59:14 +0530

    fix: fixed the no consumer error by adding the addConsumer() and also getting back the desired revert error

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-import {CreateSubscription} from "./Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
+
 
 contract DeployRaffle is Script {
     function run() external {
+        deployContract();
     }
 
     function deployContract() public returns(Raffle, HelperConfig){
@@ -19,6 +21,8 @@ contract DeployRaffle is Script {
             CreateSubscription createSubscription = new CreateSubscription();
             (config.subscriptionId, config.vrfCoordinator) = createSubscription.createSubscription(config.vrfCoordinator);
             // fund subscription
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(config.vrfCoordinator, config.subscriptionId, config.link);
         }
 
         vm.startBroadcast();
@@ -31,6 +35,11 @@ contract DeployRaffle is Script {
             config.callbackGasLimit
         );
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        //dont need to add broadcast...
+        addConsumer.addConsumer(address(raffle), config.vrfCoordinator, config.subscriptionId);
+
         return (raffle, helperConfig)                                 ;
     }
 }
